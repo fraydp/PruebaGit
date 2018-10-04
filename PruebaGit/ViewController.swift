d248f689aec61e647e92e50be09281e6b914f23a
Fray Pineda <fraypineda@imac-de-fray.local>
2018-10-04 13:37:04 -0400

    Lo subi a github

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -8,10 +8,12 @@ class ViewController: UIViewController {
        print("Este es un proyecto de ejemplo para ver el funcionamiento del repositorio git ")
         let today = NSDate()
         print(today)
+        print("Voy a subir esta linea de codigo a github directamente desde xcode")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
