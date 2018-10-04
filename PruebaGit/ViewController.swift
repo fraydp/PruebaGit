989d75e3d8e7f94acbd224b5bbc5aaf5f4694c1d
Fray Pineda <fraypineda@imac-de-fray.local>
2018-10-04 11:36:03 -0400

    Con fecha

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -6,6 +6,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        print("Este es un proyecto de ejemplo para ver el funcionamiento del repositorio git ")
+        let today = NSDate()
+        print(today)
     }
 
     override func didReceiveMemoryWarning() {
